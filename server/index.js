42d2756d5aefe7b415b6cf70869b191d6d649643
Rachel Tublitz <tublitzed@gmail.com>
2016-12-28 21:38:50 -0500

    Organize app dirs a little better

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
 var express = require('express')
+var path = require('path')
 var app = express()
 var SERVER_PORT = 4000;
+var ROOT_PATH = __dirname + '/../';
+var VIEWS_PATH = ROOT_PATH + '/app/views';
+
+app.set('views', '../views');
+app.set('views engine', 'html');
 
 app.get('/', function(req, res) {
-  res.send('Hello, world!')
+  res.sendFile('index.html', { root: VIEWS_PATH });
 })
-
 
 app.get('/yo', function(req, res) {
   res.send('yo!')
 })
+
+app.use('/public', express.static(ROOT_PATH + '/public'));
 
 var server = app.listen(SERVER_PORT, function(){
   console.log('server running at http://localhost:' + SERVER_PORT)
